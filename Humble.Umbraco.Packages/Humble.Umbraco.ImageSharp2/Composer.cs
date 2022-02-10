cf6350ca017f0c048c35d01bfe9e760d36bd26df
Mark Drake <markusdrake@gmail.com>
2022-02-10 18:48:51 -0500

    upgrade to umbraco 9.3.0 across all projects

---
+++ Humble.Umbraco.Packages/Humble.Umbraco.ImageSharp2/Composer.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Upgrade and replace Umbraco v9's implementation of ImageSharp.Web to 2.0.
+ * 
+ * Ref: https://our.umbraco.com/Documentation/Implementation/Composing/
+ */
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Web.Common;
+
+namespace Humble.Umbraco.ImageSharp2
+{
+	internal class Composer : IComposer
+	{
+		public void Compose(IUmbracoBuilder builder)
+		{
+			//builder.AddUmbracoImageSharp2();
+		}
+	}
+}
